37ce36dcf9d8221aac12f8167d234db4ba7cda56
Juan Lorthiois <lor18771@uvg.edu.gt>
2022-04-06 13:37:41 -0600

    implementacion del programa del hospital utilizando PriorityQueue del JCF

---
+++ src/Paciente.java
@@ -0,0 +1,25 @@
+public class Paciente implements Comparable<Paciente>{
+
+	public String nombre;
+	public String sintoma;
+	public String codigo;
+	
+	
+	
+	public int compareTo(Paciente p) {
+		
+		return this.codigo.compareTo(p.codigo);
+	}
+
+	
+	public Paciente(String nombre, String sintoma, String codigo ) {
+		
+		this.nombre = nombre;
+		this.sintoma = sintoma;
+		this.codigo = codigo;
+		
+		
+	}
+
+	
+}
